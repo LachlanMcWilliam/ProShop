f8cf664d6de67c50a633f694c149d1371f1df071
Lachlan <mcwilliam01@gmail.com>
2021-01-19 08:08:07 +0000

    updated naming coventions (labeled deployments as services) and added mongo to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,9 @@ import express from "express";
 import { getProductRouter } from "./routes/get-product.js";
 import { getAllProductsRouter } from "./routes/get-all-products.js";
 
-let PORT = 0;
-
-if (process.env.SERVICE_PORT) {
-  PORT = process.env.SERVICE_PORT;
-} else {
-  throw new Error("Please define the 'SERVICE_PORT' environment variable");
-}
-
 const app = express();
 
 app.use(getProductRouter);
 app.use(getAllProductsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Products server running on port: ${PORT}`);
-});
+export { app };
